996633e5167f2a367d46f12f6b26645f600adbf2
ManrajSidhu123 <sidhu-m3@webmail.uwinnipeg.ca>
2021-12-02 10:34:40 -0600

    Initial sharing of project

---
+++ FrequencyCounter1.java
@@ -0,0 +1,94 @@
+import java.util.Scanner;
+import javax.swing.*;
+import java.io.*;
+import java.text.NumberFormat;
+import java.util.Random; 
+
+/** ***************************************************
+ *  Name:           Manraj 
+ *  Class:          CS30S
+ * 
+ *  Assignment:     A1.5 
+ * 
+ *  Description:    Frequency counter using arrays 
+ * 
+ *************************************************************/
+
+public class FrequencyCounter1{
+
+    public static void main(String[] args) throws IOException{
+    // ***** constants *******
+    final int MAX = 1000; 
+    
+    // ***** variables *****
+    
+        String banner = "";             // output banner
+        String prompt = "";             // prompt for user input
+        
+        String strin = "";              // string fro keyboard input
+        String strout = "";             // string for formatted output
+        
+        String delim = "[ ]+";          // delimiter for splitting input records
+        String[] tokens = null;         // used to split input records
+        
+        // a new line character that works on every computer system
+        String nl = System.lineSeparator();
+        
+        // array 
+        int random = 0; 
+        int a = 1000; 
+        
+    
+    // ***** objects *****
+    
+        //Scanner scanner = new Scanner(System.in);
+        //NumberFormat currency = NumberFormat.getCurrencyInstance();
+        
+        // file io buffers for reading and writing to text files
+        
+        //BufferedReader fin = new BufferedReader(new FileReader("filename.txt"));
+        PrintWriter fout = new PrintWriter(new BufferedWriter(new FileWriter("Printing nuber.txt")));
+    
+        Random rnd = new Random(); 
+    // ***** print banners *****
+    
+        banner = "*****************************" + nl;
+        banner += "Name:        Manraj" + nl;
+        banner += "Class:       CS30S" + nl;
+        banner += "Assignment:  A1.5" + nl;
+        banner += "*****************************" + nl + nl;
+        
+        System.out.println(banner);
+        fout.print(banner);
+    
+    // ***** Get Input *****
+    
+        // prompt for input
+        // read input from keyboard
+        // echo input back to console window
+      for(int i =  0; i < a; i++){
+       // random number 
+       random = rnd.nextInt(50) + 0; 
+    
+       // printing numbers disk file 
+       fout.println(random); 
+      }// end for loop  
+      
+    
+    // ***** Main Processing *****
+    
+    // ***** Print Formatted Output *****
+    
+    // ***** Closing Message *****
+    
+        System.out.println();
+        System.out.println("end of processing");
+        fout.println("End of Processing");
+        
+    // **** close io buffers *****
+    
+        //fin.close();
+        fout.close();
+    } // end main 
+    
+} // end FormatTemplate
---
+++ FrequencyCounter2.java
@@ -0,0 +1,90 @@
+import java.util.Scanner;
+import javax.swing.*;
+import java.io.*;
+import java.text.NumberFormat;
+
+/** ***************************************************
+ *  Name:           Manraj
+ *  Class:          CS30S
+ * 
+ *  Assignment:     Part 2 of A1.5 
+ * 
+ *  Description:    Count frequency of numbers from 1-50
+ * 
+ *************************************************************/
+
+public class FrequencyCounter2{
+
+    public static void main(String[] args) throws IOException{
+    // ***** constants *******
+    
+    final int SIZE = 1000; 
+
+    // ***** variables *****
+    
+        String banner = "";             // output banner
+        String prompt = "";             // prompt for user input
+        
+        String strin = "";              // string fro keyboard input
+        String strout = "";             // string for formatted output
+        
+        String delim = "[ ]+";          // delimiter for splitting input records
+        String[] tokens = null;         // used to split input records
+        
+        BufferedReader fin = null;       // input buffer 
+        
+        // a new line character that works on every computer system
+        String nl = System.lineSeparator();
+        
+        // variables 
+        int[] NArray = new int[SIZE]; 
+         
+
+
+ 
+    // ***** objects *****
+    
+        //Scanner scanner = new Scanner(System.in);
+        //NumberFormat currency = NumberFormat.getCurrencyInstance();
+        
+        // file io buffers for reading and writing to text files
+        try{
+           fin = new BufferedReader(new FileReader("RandomNumbers.txt"));
+        }// end try 
+        catch(FileNotFoundException e){
+           System.out.println("File not found"); 
+          fin = null; 
+        }// end catch
+        //PrintWriter fout = new PrintWriter(new BufferedWriter(new FileWriter("outfle.txt")));
+    // ***** print banners *****
+    
+        banner = "*****************************" + nl;
+        banner += "Name:        Manraj" + nl;
+        banner += "Class:       CS30S" + nl;
+        banner += "Assignment:  A1.5 Part 2" + nl;
+        banner += "*****************************" + nl + nl;
+        
+        System.out.println(banner);
+        //fout.print(banner);
+    
+    // ***** Get Input *****
+    
+        // prompt for input
+        // read input from keyboard
+        // echo input back to console window
+    
+    // ***** Main Processing *****
+
+    // ***** Closing Message *****
+    
+        System.out.println();
+        System.out.println("end of processing");
+        //fout.println("End of Processing");
+        
+    // **** close io buffers *****
+    
+        //fin.close();
+        //fout.close();
+    } // end main 
+    
+} // end FormatTemplate
